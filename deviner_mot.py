b6e68e9f5e8472570a2e8c926e4f37870ab71018
Jacques Maubert <84455706+kejatp@users.noreply.github.com>
2024-05-06 12:15:26 +0200

    Update deviner_mot.py

--- deviner_mot.py
+++ deviner_mot.py
@@ -3,22 +3,18 @@ import difflib
 import tkinter as tk
 from tkinter import messagebox
 
-# Assurez-vous d'avoir téléchargé le package 'punkt' de nltk
-import nltk
-nltk.download('punkt')
-nltk.download('words')
-from nltk.corpus import words
-
-# Charger le dictionnaire français
-mots_francais = words.words()
+# Lecture du fichier de mots français
+with open("./liste_francais/liste_francais.txt", "r") as file:
+    french_words_list = [line.strip().lower() for line in file]
 
 class DevinerMotApp:
     def __init__(self, master):
         self.master = master
         self.master.title("Jeu de deviner le mot")
-        self.master.geometry("300x200")
+        self.master.geometry("300x250")
 
-        self.mot_a_deviner = random.choice(mots_francais)
+        self.mot_a_deviner = random.choice(french_words_list)
+        self.essais = 10
 
         self.label_mot = tk.Label(master, text="Devinez le mot !")
         self.label_mot.pack()
@@ -31,14 +27,19 @@ class DevinerMotApp:
 
     def deviner(self):
         mot_entre = self.entry_mot.get()
+        self.essais -= 1
+
         similarite = difflib.SequenceMatcher(None, self.mot_a_deviner, mot_entre).ratio()
         messagebox.showinfo("Résultat", f"La similarité entre les deux mots est : {similarite}")
 
-        if mot_entre.lower() == self.mot_a_deviner.lower():
+        if mot_entre.lower() == self.mot_a_deviner:
             messagebox.showinfo("Bravo !", "Vous avez deviné le mot correctement.")
-            self.mot_a_deviner = random.choice(mots_francais)
+            self.master.quit()
+        elif self.essais == 0:
+            messagebox.showinfo("Désolé !", f"Vous avez utilisé tous vos essais. Le mot était '{self.mot_a_deviner}'.")
+            self.master.quit()
         else:
-            messagebox.showinfo("Essayez encore !", "Essayez de deviner un autre mot.")
+            messagebox.showinfo("Essayez encore !", f"Il vous reste {self.essais} essais.")
 
 def main():
     root = tk.Tk()
